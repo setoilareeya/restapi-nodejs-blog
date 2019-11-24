7aa7a746c2cae18a9808ca15336697bf2442c37d
setoilareeya <setoilareeya@gmail.com>
2019-11-24 17:24:26 +0700

    restapi-nodejs-blog
    restapi-nodejs-blog

---
+++ README.md
@@ -0,0 +1,32 @@
+# DevConnector 2.0
+
+
+## Quick Start
+
+```
+# change default.json file in config folder
+
+# this file is located in config/default.json
+
+# add uri of your mongodb connection for example
+
+ "mongoURI": "mongodb://localhost/dev-social",
+ 
+```
+
+```bash
+# Install server dependencies
+npm install
+
+# Install client dependencies
+cd client
+npm install
+
+# Run both Express & React from root
+npm run dev
+
+# Build for production
+cd client
+npm run build
+
+
---
+++ models/Post.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
